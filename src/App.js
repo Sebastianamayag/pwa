e6835ede4c623decf49c7b428d635fa41d4e02e7
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2021-10-11 18:37:29 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  useEffect(() => {
+    traerData();
+  }, [])
+
+  const traerData=async()=>{
+    const resp=await fetch('https://jsonplaceholder.typicode.com/users');
+    const body=await resp.json();
+    setData(body);
+    setCargando(true);
+  }
+
   return (
-    <div style={{display:'flex',flex:1,height:'100%',justifyContent:'center',alignItems:'center',backgroundColor:'darkblue'}}>
-      <h1 style={{color:'whitesmoke'}} >Hecho por Juan Sebastian Amaya Gómez</h1>
+    <div style={{display:'flex',flex:1,height:'100%',alignItems:'center'}}>
+      {
+        !cargando?
+        (
+          <div>
+            <p>Cargando.....</p>
+          </div>
+        ):
+        (
+          <div>
+            <table>
+              <thead>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </thead>
+                {
+                  data.map((dats,key)=>
+                  (
+                    <tbody>
+                      <td>{dats.name}</td>
+                      <td>{dats.username}</td>
+                      <td>{dats.email}</td>
+                      <td>{dats.phone}</td>
+                    </tbody>
+                  )
+                  )
+                }
+            </table>
+          </div>
+        )
+      }
     </div>
   );
 }
